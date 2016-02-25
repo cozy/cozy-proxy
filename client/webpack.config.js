71eadfd3cf19914348d2c76fddb3b0ae87f1f013
M4D <code@m4dz.net>
2016-02-25 14:20:04 +0100

    [refactor] make webpack.config more readable

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,17 +1,73 @@
 var path = require('path');
 
-var webpack           = require('webpack');
+var webpack = require('webpack');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyPlugin        = require('copy-webpack-plugin');
 var AssetsPlugin      = require('assets-webpack-plugin');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
+// use the `OPTIMIZE` env VAR to switch from dev to production build
 var optimize = process.env.OPTIMIZE === 'true';
 
-var autoprefixer = require('autoprefixer')(['last 2 versions']);
-var mqpacker     = require('css-mqpacker');
+/**
+ * Loaders used by webpack
+ *
+ * - CSS and images files from `vendor` are excluded
+ * - stylesheets are optimized via cssnano, minus svgo and autoprefixer that are
+ * customized via PostCSS
+ * - images are cache-busted in production build
+ */
+var cssOptions = optimize? 'css?-svgo&-autoprefixer&-mergeRules!postcss':'css';
+var imgPath = 'img/' + '[name]' + (optimize? '.[hash]': '') + '.[ext]';
+var loaders = [
+    {
+        test: /\.coffee$/,
+        loader: 'coffee'
+    },
+    {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style', cssOptions + '!stylus')
+    },
+    {
+        test: /\.css$/,
+        exclude: /vendor/,
+        loader: ExtractTextPlugin.extract('style', cssOptions)
+    },
+    {
+        test: /\.jade$/,
+        loader: 'jade'
+    },
+    {
+        test: /\.json$/,
+        loader: 'json'
+    },
+    {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        exclude: /vendor/,
+        loader: 'file?name=' + imgPath
+    }
+];
 
+/**
+ * Configure Webpack's plugins to tweaks outputs:
+ *
+ * all builds:
+ * - ExtractTextPlugin: output CSS to file instead of inlining it
+ * - CommonsChunkPlugin: push to _main_ file the common dependencies
+ * - CopyPlugin: copy assets to public dir
+ *
+ * prod build:
+ * - AssetsPlugin: paths to cache-busted's assets to read them from server
+ * - DedupePlugin
+ * - OccurenceOrderPlugin
+ * - UglifyJsPlugin
+ * - DefinePlugin: disable webpack env dev vars
+ *
+ * dev build:
+ * - BrowserSyncPlugin: make hot reload via browsersync exposed at
+ *   http://localhost:3000, proxified to the server app port
+ */
 var plugins = [
     new ExtractTextPlugin(optimize? 'app.[hash].css' : 'app.css'),
     new webpack.optimize.CommonsChunkPlugin({
@@ -32,7 +88,7 @@ if (optimize) {
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
-            mangle:   true,
+            mangle: true,
             compress: {
                 warnings: false
             },
@@ -52,6 +108,24 @@ if (optimize) {
     ]);
 }
 
+/**
+ * PostCSS Config
+ *
+ * - autoprefixer to add vendor prefixes for last 2 versions
+ * - mqpacker to bring together all MQ rule's set
+ */
+var postcss = [
+    require('autoprefixer')(['last 2 versions']),
+    require('css-mqpacker')
+];
+
+/**
+ * Webpack config
+ *
+ * - output to `public` dir
+ * - cache-bust assets when build for production
+ */
+
 module.exports = {
     entry: './app/initialize',
     output: {
@@ -65,35 +139,8 @@ module.exports = {
     debug: !optimize,
     devtool: 'source-map',
     module: {
-        loaders: [
-            {
-                test: /\.coffee$/,
-                loader: 'coffee'
-            },
-            {
-                test: /\.styl$/,
-                loader: ExtractTextPlugin.extract('style', optimize? 'css?-svgo&-autoprefixer&-mergeRules!postcss!stylus' : 'css!stylus')
-            },
-            {
-                test: /\.css$/,
-                exclude: /vendor/,
-                loader: ExtractTextPlugin.extract('style', optimize? 'css?-svgo&-autoprefixer&-mergeRules!postcss' : 'css')
-            },
-            {
-                test: /\.jade$/,
-                loader: 'jade'
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
-            },
-            {
-                test: /\.(png|gif|jpe?g|svg)$/i,
-                exclude: /vendor/,
-                loader:  'file?name=img/' + (optimize? '[name].[hash].[ext]' : '[name].[ext]')
-            }
-        ]
+        loaders: loaders
     },
     plugins: plugins,
-    postcss: [autoprefixer, mqpacker]
+    postcss: postcss
 };

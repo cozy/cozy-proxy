adb439c2614b62c3255125cdf1f77ee16293b575
Noëlie Andrieu <noelieandrieu@gmail.com>
2016-10-13 20:42:21 +0200

    [test] add password

--- client/test/lib/onboarding.js
+++ client/test/lib/onboarding.js
@@ -875,11 +875,7 @@ describe('Onboarding.Step', () => {
             assert(callback2.calledWith(step));
         });
     });
-<<<<<<< da96882f5b8a010ad48cf9608ab4efe6207d6392
 
-
-=======
->>>>>>> [test] config.password
     describe('#submit', () => {
         it('should call save', () => {
             // arrange

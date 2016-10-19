80597382cbfed6c3df5af339d2ae54b4f9cafbea
Grégory Le Garec <gregory.legarec@gmail.com>
2016-10-19 17:09:40 +0200

    Update password step tests to work with async Step.save method

--- client/test/config/steps/password.js
+++ client/test/config/steps/password.js
@@ -41,18 +41,24 @@ describe('Step: password', () => {
     });
 
 
-    describe('#submit', () => {
+    describe('#save', () => {
 
         it('should send POST request', () => {
             // Define global.jQUery
             // so that configPassword could use it
-            global.jQuery.post = sinon.spy()
+            sinon.stub(global.jQuery, 'post');
             let data = { email: '', password: 'plop', timezone: TimeZones[0]}
-            PasswordConfig.submit(data);
+            PasswordConfig.save(data);
 
             data = JSON.stringify(data);
+
+            let spyArgument = global.jQuery.post.getCalls(0)[0].args[0];
+
             assert(global.jQuery.post.calledOnce);
-            assert(global.jQuery.post.calledWith('/register', data));
+            assert.equal('/register', spyArgument.url);
+            assert.deepEqual(data, spyArgument.data);
+
+            global.jQuery.post.restore();
         });
 
     });

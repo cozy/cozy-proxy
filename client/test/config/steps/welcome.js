5eb44d8939a6ba7569a54752749e8359ef5335fc
Raphaël Rougeron <goldoraf@gmail.com>
2016-11-17 15:16:59 +0100

    [test] Added tests for the welcome step

---
+++ client/test/config/steps/welcome.js
@@ -0,0 +1,34 @@
+'use strict';
+let assert = require('chai').assert;
+let fetchMock = require('fetch-mock');
+
+describe('Step: welcome', () => {
+	let jsdom;
+    let WelcomeConfig;
+
+    before(function () {
+        jsdom = require('jsdom-global')();
+        WelcomeConfig = require('../../../app/config/steps/welcome.coffee');
+    });
+
+    after(function () {
+        jsdom();
+    });
+
+    afterEach(() => {
+        fetchMock.restore();
+    });
+
+    describe('#save', () => {
+
+        it('should send POST request', () => {
+            fetchMock.post('*', 200);
+            WelcomeConfig.save();
+            assert.ok(fetchMock.called());
+            assert.equal('/register', fetchMock.lastUrl());
+            assert.deepEqual(JSON.stringify({onboardedSteps: ['welcome']}),
+                             fetchMock.lastOptions().body);
+        });
+
+    });
+});
